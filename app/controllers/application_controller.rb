c758c8c02aa6ddd4f46045117e5814cda36a62c3
rubygitflow <nrubygitflow@gmail.com>
2024-02-24 07:50:56 +0600

    delete the USER_UUID history

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,10 +22,7 @@ class ApplicationController < ActionController::API
   def current_user
     raise ActionController::InvalidAuthenticityToken unless http_user_uuid
 
-    # @current_user ||= Client.find_by(id: session_user_uuid)
     @current_user ||= Client.find_by(id: http_user_uuid)
-    # @current_user ||= Client.find_by(id: payload[:user_uuid])
-    # @current_user ||= Client.find_by(id: env_user_uuid)
     raise MarketplaceAggregator::ForbiddenError unless @current_user
 
     @current_user
@@ -38,32 +35,7 @@ class ApplicationController < ActionController::API
 
   private
 
-  # def env_user_uuid
-  #   ENV['USER_UUID']
-  # end
-
-  # def session_user_uuid
-  #   session['user_uuid']
-  # end
-
   def http_user_uuid
     request.headers['HTTP_USER']
   end
-
-  # def payload
-  #   payload_header[0].deep_symbolize_keys
-  # end
-
-  # def payload_header
-  #   JWT.decode(token,
-  #              Settings.jwt.secret_key,
-  #              true,
-  #              { algorithm: 'HS256' })
-  # rescue JWT::ExpiredSignature
-  #   not_authorized
-  # end
-
-  # def not_authorized
-  #   render(json: { error: 'Not Authorized' }, status: :unauthorized)
-  # end
 end
