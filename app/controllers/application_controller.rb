776a54dc0f3fc7d30eba58715054db7f2d22943e
rubygitflow <nrubygitflow@gmail.com>
2024-02-27 16:11:51 +0600

    follow the style guide

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,22 +20,22 @@ class ApplicationController < ActionController::API
   end
 
   def current_user
-    raise MarketplaceAggregator::UnauthorizedError unless http_user_uuid
+    raise MarketplaceAggregator::UnauthorizedError unless fetch_user_uuid
 
-    @current_user ||= Client.find_by(id: http_user_uuid)
+    @current_user ||= Client.find_by(id: fetch_user_uuid)
     raise MarketplaceAggregator::ForbiddenError unless @current_user
 
     @current_user
   end
 
   def marketplace(attr)
-    @marketplace ||= Marketplace.find_by_name(attr.capitalize) ||
-                     Marketplace.find_by_label(attr)
+    @marketplace = Marketplace.find_by_name(attr.capitalize) ||
+                   Marketplace.find_by_label(attr)
   end
 
   private
 
-  def http_user_uuid
+  def fetch_user_uuid
     request.headers['HTTP_USER']
   end
 end
